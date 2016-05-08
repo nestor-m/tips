305fdbcff35844532ba9476a09953a962cebde40
Guido Pujadas <guidopujadas@gmail.com>
2016-05-08 18:23:45 -0300

    rutas

--- tests/backend/rutas.test.js
+++ tests/backend/rutas.test.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var Idea = require("../../models/Ideas.js");
 require("../../models/Usuarios.js");
 require("../../models/Actividades.js");
@@ -19,6 +20,29 @@ describe("router Ideas", function() {
 	before(function(done) {
 		mockgoose(mongoose).then(function() {
 			mongoose.connect("mongodb://localhost/testingRutas");
+=======
+var express = require('express');
+var request = require('supertest');
+var should = require("chai").should();
+var mongoose = require("mongoose");
+var mockgoose = require("mockgoose");
+
+var Idea = require("../../models/Ideas.js");
+require("../../models/Usuarios.js");
+require("../../models/Actividades.js");
+require("blanket")
+
+var app = express();
+app.use('/', require('../../routes/index'));
+
+
+
+describe("rutas de ideas", function(){
+
+	before(function(done) {
+		mockgoose(mongoose).then(function() {
+			mongoose.connect("mongodb://localhost/rutasTests");
+>>>>>>> Stashed changes
 			done();
 		})
 	});
@@ -27,6 +51,7 @@ describe("router Ideas", function() {
 		mockgoose.reset(done);
 	});
 
+<<<<<<< Updated upstream
 	var ideanueva;
 	var ideaeliminada;
 
@@ -45,10 +70,30 @@ describe("router Ideas", function() {
   		ideaeliminada.estado = "ELIMINADA";
   		ideaeliminada.postulante = "alguien";
   		ideaeliminada.save();
+=======
+	var idea;
+
+	beforeEach(function(done) {
+		idea = new Idea();
+		idea.titulo = "Nueva Idea";
+  		idea.descripcion = "descripcion de nueva idea";
+  		idea.autor =  "Guido";
+  		idea.postulante = "postulantePorDefecto";
+  		idea.save();
+
+  		idea2 = new Idea();
+		idea2.titulo = "Nueva Idea";
+  		idea2.descripcion = "descripcion de nueva idea";
+  		idea2.autor =  "Guido";
+  		idea2.estado = "ELIMINADA";
+  		idea2.postulante = "postulantePorDefecto";
+  		idea2.save();
+>>>>>>> Stashed changes
 
   		done();
 	});
 
+<<<<<<< Updated upstream
 	describe("GET /ideas", function() {
 		it("debe retornarme la lista de ideas no eliminadas", function(done) {
 			request(app)
@@ -73,3 +118,34 @@ request(app)
 should.not.exist(err);
 response.body.should.have.property("upvotes").equal(13);
 */
+=======
+	it("get /ideas debe retornar el total de las ideas no eliminadas", function(done){
+		request(app)
+			.get("/ideas")
+			.expect(200)
+			.end(function(err, response){
+				should.not.exist(err);
+				response.body.should.have.lengthOf(1);
+				done();
+			});
+	});
+
+	//como se arreglaba el tema del Unathrorized? y que onda los pedidos andidados?
+	it("post /ideas debe crear una nueva idea con los valores determinados", function(done){
+		request(app)
+			.post("/ideas")
+			.expect(200)
+			.end(function(err, response){
+				should.not.exist(err);
+				request(app)
+				.get("/ideas")
+				.expect(200)
+				.end(function(err,res){
+					res.body.should.have.lengthOf(2);
+				});
+				done();
+			});
+	});
+});
+
+>>>>>>> Stashed changes
