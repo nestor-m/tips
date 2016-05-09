8c50fc98cdaa8f632cd07004f765a7d21183523c
nestor-m <nestorgabriel2008@gmail.com>
2016-05-09 01:46:02 -0300

    Agrego funcionalidad de comentar ideas con un test

--- public/javascripts/controllers/detallesCTRL.js
+++ public/javascripts/controllers/detallesCTRL.js
@@ -1,6 +1,32 @@
 var app = angular.module('ideasTIP');
 
-app.controller('detallesCRTL', ['$scope','$state','idea', function($scope, $state, idea){
-  $scope.tituloIdea = idea.titulo;
-  $scope.descripcionIdea = idea.descripcion;
-}])
+app.controller('detallesCTRL', ['$scope','$http','idea','authFactory',
+	function($scope,$http, idea, authFactory)
+	{
+  $scope.idea = idea;
+
+  // $scope.idea.comentarios = [
+	 //  {"autor":"pepe","contenido":"Me parece una muy buena idea","fecha":new Date()},
+	 //  {"autor":"pepe","contenido":"Me parece una muy buena idea","fecha":new Date()},
+	 //  {"autor":"pepe","contenido":"Me parece una muy buena idea","fecha":new Date()},
+	 //  {"autor":"pepe","contenido":"Me parece una muy buena idea","fecha":new Date()}
+  // ];
+
+  $scope.comentar = function(){
+  	var comentario = {
+  		autor: $scope.usuario.usuario,  		
+  		contenido: $scope.comentario
+  	};
+  	return $http.post('/ideas/'+ $scope.idea._id + '/comentar',
+  		comentario,
+  		{
+  			headers: { Authorization: 'Bearer ' + authFactory.getToken() }
+  		}
+  	).success(function(data){//espero que me retorne la lista de todos los comentarios
+  		$scope.idea.comentarios = data;
+  	},function(error){
+  		alert("No se pudo realizar el comentario \n" + error);
+  	});
+  };
+
+}]);
